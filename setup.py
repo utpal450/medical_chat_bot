2b6632585c666be1cc10134d5bd69e4eb2498199
Utpal Sarkar <utpalsarkar75257@gmail.com>
2026-09-01 19:38:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical_chat_bot",
+    version="0.1.0",
+    author="Utpal Sarkar",
+    author_email="utpalsatkar75257@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
