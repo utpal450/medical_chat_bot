a2e0762b1e57394ed44e61e7228f6d31a72610d8
Utpal Sarkar <utpalsarkar75257@gmail.com>
2026-09-01 19:09:20 +0530

    folder structur

---
+++ template.sh
@@ -0,0 +1,18 @@
+# create directory
+mkdir -p src
+mkdir -p research
+
+# create files
+touch src/main.py
+touch src/helpers.py
+touch src/prompts.py
+
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+
+touch requirements.txt
+
+
+echo "Project structure created successfully."
+
